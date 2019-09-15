a49135afae4311fab05b1b9c2715deb32b7ebe93
mygotv <mygotv@gmail.com>
2019-09-15 01:26:08 -0700

    End of day check in.

--- MVC/MVC/Controllers/HomeController.cs
+++ MVC/MVC/Controllers/HomeController.cs
@@ -3,15 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MVC.Models;
 
 namespace MVC.Controllers
 {
     public class HomeController : Controller
     {
         // GET: Home
-        public ActionResult GetCourses()
+        public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult GetCourses()
+        {
+            Courses coursesRaw = new Courses();
+            var temp = coursesRaw.getCourses();
+
+            CoursesDto courses = (CoursesDto)temp;
+
+            return View(courses.Courses as IEnumerable<Courses>);
         }
     }
 }
---
+++ MVC/MVC/Models/Courses.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Script.Serialization;
+
+namespace MVC.Models
+{
+    public class Courses
+    {
+        public string title { get; set; }
+        public string description { get; set; }
+        public int credits { get; set; }
+
+        public Object getCourses()
+        {
+            string url = "https://www2.bellevuecollege.edu/data/api/v1/courses/engl";
+            //synchronous client
+            var client = new WebClient();
+            var content = client.DownloadString(url);
+            var serializer = new JavaScriptSerializer();
+            var jsonContent = serializer.Deserialize<CoursesDto>(content);
+            return jsonContent;
+        }
+    }
+
+    public class CoursesDto
+    {
+        public List<Courses> Courses { get; set; }
+    }
+}
