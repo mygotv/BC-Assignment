02ffdb9741b8a222ebeca13af35d7ae01bbc03d1
mygotv <mygotv@gmail.com>
2019-09-14 19:01:31 -0700

    Initial working version

---
+++ Weather/Weather/Models/Course.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Weather.Models
+{
+    public class Course
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int  Credits { get; set; }
+
+    }
+}
---
+++ Weather/Weather/Models/Courses.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Script.Serialization;
+
+
+namespace Weather.Models
+{
+    public class Courses
+    {
+        
+        public Object getCourses()
+        {
+            string url = "https://www2.bellevuecollege.edu/data/api/v1/courses/engl";
+            //synchronous client
+            var client = new WebClient();
+            var content = client.DownloadString(url);
+            var serializer = new JavaScriptSerializer();
+            var jsonContent = serializer.Deserialize<CourseDto>(content);
+            return jsonContent;
+        }
+    }
+
+    public class CourseDto
+    {
+       public List<Course> Courses { get; set; }
+    }
+}
